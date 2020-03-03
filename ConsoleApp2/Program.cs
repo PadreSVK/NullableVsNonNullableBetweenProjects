9e6497ed1ddb36d144078f4e9ec21c477c1e1187
Patrik Svikruha <patrik.svikruha@gmail.com>
2020-03-03 22:49:35 +0100

    add nonnullable console app

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EnableNullable;
+using NonNullable;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var ttt = new SuperClass();
+
+            ttt.SuggestedTimes.Any();
+
+            Enumerable.Any(ttt.SuggestedTimes);
+            ttt.SuggestedTimes.First();
+            ttt.SuggestedTimes.Any();
+
+            ttt.SuggestedTimes.TestNonNullable();
+            ttt.SuggestedTimes.TestNullable();
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class SuperClass
+    {
+        public ICollection<string> SuggestedTimes { get; }
+    }
+}
